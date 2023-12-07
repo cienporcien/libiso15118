db684aff128688c8a97e7a22fc985d785396abf2
Roger Bedell <rogerbedell@hotmail.com>
2023-12-07 19:25:06 +0100

    ACDP include files

---
+++ include/iso15118/message/acdp_vehicle_positioning.hpp
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2023 Pionix GmbH and Contributors to EVerest
+#pragma once
+
+#include "common.hpp"
+
+namespace iso15118::message_20 {
+
+struct ACDP_VehiclePositioningRequest {
+    Header header;
+    // EVMobilityStatus, boolean
+    int EVMobilityStatus;
+    // EVPositioningSupport, boolean
+    int EVPositioningSupport;
+};
+
+struct ACDP_VehiclePositioningResponse {
+
+    ACDP_VehiclePositioningResponse() : processing(Processing::Ongoing){};
+
+    Header header;
+    ResponseCode response_code;
+
+    Processing processing;
+    int EVSEPositioningSupport;
+    // EVRelativeXDeviation, short (base: int)
+    int16_t EVRelativeXDeviation;
+    // EVRelativeYDeviation, short (base: int)
+    int16_t EVRelativeYDeviation;
+    // ContactWindowXc, short (base: int)
+    int16_t ContactWindowXc;
+    // ContactWindowYc, short (base: int)
+    int16_t ContactWindowYc;
+    // EVInChargePosition, boolean
+    int EVInChargePosition;
+};
+
+} // namespace iso15118::message_20
